521f7931e4be98e45ab8cab71f7eef8e1ca26eb2
Ayush <ayush15may@gmail.com>
2019-07-26 18:31:59 +0530

    adding puppeteer for e2e

---
+++ e2e/puppeteer.conf.js
@@ -0,0 +1,9 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://google.com");
+  await page.screenshot({ path: "e2e/artifacts/google.png" });
+  await browser.close();
+})();
