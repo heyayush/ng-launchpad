e814914a1ba49d364aea61affe7e01490953bbe6
Ayush <ayush15may@gmail.com>
2019-08-21 15:06:48 +0530

    adding linting and prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed, async } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppComponent } from "./app.component";
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   beforeEach(async(() => {
     return TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -10,7 +10,7 @@ describe("AppComponent", () => {
     }).compileComponents();
   }));
 
-  test("should create the app", async(() => {
+  test('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -19,15 +19,15 @@ describe("AppComponent", () => {
   test(`should have as title 'ng-launchpad'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("ng-launchpad");
+    expect(app.title).toEqual('ng-launchpad');
   }));
 
-  test("should render title in a h1 tag", async(() => {
+  test('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to ng-launchpad!"
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to ng-launchpad!'
     );
   }));
 });
